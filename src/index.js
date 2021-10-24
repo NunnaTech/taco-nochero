b7bc1e4a65909fe691aba304a0628490b4e742e1
JairDavid <jairvasmar@gmail.com>
2021-10-24 10:05:42 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "../src/assets/css/tailwind.css";
-import Dashboard from './views/landing_pages/Dashboard';
+import Menu from './views/landing_pages/Menu'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dashboard />
+    <Menu />
   </React.StrictMode>,
   document.getElementById('root')
 );
