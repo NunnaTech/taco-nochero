f4015bc003b974f231545c061b0f7c9d2a2804b9
AlexisLoya <alexloy117@gmail.com>
2021-10-25 01:03:04 -0500

    fix resposive svg in 403 and 404

--- src/views/error_pages/403.jsx
+++ src/views/error_pages/403.jsx
@@ -4,20 +4,22 @@ import TacoButton from "../../commons/TacoButton";
 
 export default function Page403() {
   return (
-    <div className="flex h-screen   items-center justify-center ">
+    <div className="flex h-screen  items-center justify-center ">
       <div className="card  text-center text-white shadow-sm p-5">
         <h1 className="text-center text-9xl font-bold">403</h1>
-        <div className=" m-1">
-          <h2 className="text-4xl  font-bold">No tienes acceso a este sitio</h2>
-          <div className="justify-center flex-col">
-              <img className="my-5"  src={Alarm} alt="Alarm" />
-                <TacoButton
-                  moreClasses="my-5"
-                  bgColor="300"
-                  callBack={() => console.log("going to landing page")}
-                  text="Volver al inicio"
-                />
-          </div>
+        <div className="m-1">
+          <h2 className="text-4xl  font-bold">
+          No tienes acceso a este sitio
+          </h2>
+        </div>
+        <div className="justify-center flex-col flex items-center my-5 mx-auto">
+          <TacoButton
+            moreClasses="w-1/2 mx-auto"
+            bgColor="300"
+            callBack={() => console.log("going to landing page")}
+            text="Volver al inicio"
+          />
+          <img className="my-10 w-80 self-center" src={Alarm} alt="Alarm" />
         </div>
       </div>
     </div>
--- src/views/error_pages/404.jsx
+++ src/views/error_pages/404.jsx
@@ -4,21 +4,21 @@ import TacoButton from "../../commons/TacoButton";
 
 export default function Page404() {
   return (
-    <div className="flex h-screen  items-center justify-center">
-      <div className="card min-h-screen text-center text-white shadow-sm">
+    <div className="flex h-screen  items-center justify-center ">
+      <div className="card  text-center text-white shadow-sm p-5">
         <h1 className="text-center text-9xl font-bold">404</h1>
-        <div className=" m-1">
+        <div className="m-1">
           <h2 className="text-4xl  font-bold">Página no encontrada</h2>
           <p className="m-1 ">La página que estás buscando no existe</p>
-          <div className="justify-center card-actions flex-col">
-              <img className="m-2 " src={BrokenTaco} alt="broken taco" />
-                <TacoButton
-                  className="m-10 p-10"
-                  bgColor="300"
-                  callBack={() => console.log("going to landing page")}
-                  text="Volver al inicio"
-                />
-          </div>
+        </div>
+        <div className="justify-center flex-col flex items-center my-5 mx-auto">
+          <TacoButton
+            moreClasses="w-1/2 mx-auto"
+            bgColor="300"
+            callBack={() => console.log("going to landing page")}
+            text="Volver al inicio"
+          />
+          <img className="my-10 w-80 self-center" src={BrokenTaco} alt="Taco" />
         </div>
       </div>
     </div>
