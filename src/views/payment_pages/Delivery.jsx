ce0644cc939df7c28868ba3750b2a26e14c9693d
AlexisLoya <alexloy117@gmail.com>
2021-10-26 21:42:50 -0500

    View structure almost done

---
+++ src/views/payment_pages/Delivery.jsx
@@ -0,0 +1,13 @@
+export default function Delivery() {
+  return (
+    <div className="flex items-center justify-center ">
+      <div className="card  text-center text-white shadow-sm p-5">
+        <h1 className="text-center text-9xl font-bold">Delivery</h1>
+        <div className="m-1">
+          <h2 className="text-4xl  font-bold">Página no encontrada</h2>
+          <p className="m-1 ">La página que estás buscando no existe</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/views/payment_pages/payment_view.jsx
+++ src/views/payment_pages/payment_view.jsx
@@ -1,80 +1,106 @@
 import React from "react";
+import { useState } from "react";
+
 import { MdDelete } from "react-icons/md";
 import TacoButton from "../../commons/TacoButton";
 import TacoInput from "../../commons/TacoInput";
 import TacoButtonIcon from "../../commons/TacoButtonIcon";
+import Details from "./Details";
+import Delivery from "./Delivery";
+import Payment from "./Payment";
+import Confirmation from "./Confirmation";
 
-export default function Dashboard() {
+export default function PaymentPage() {
+  const [step, setStep] = useState(0);
+  const onChange = (nextStep) => {
+    console.log("onChange ->", step);
+    setStep(setStep < 0 ? 0 : nextStep > 3 ? 4 : nextStep);
+  };
+  const onNext = () => (step < 0 ? onChange(step + 2) : onChange(step + 1));
+  const onGoBack = () => (step >= 1 ? onChange(step - 1) : null);
+
   return (
-    <div className="md:container w-full h-screen mx-auto text-white ">
-      <div class="bg-white h-5/6 w-full m-5 grid grid-rows-3 grid-flow-col gap-2">
-        <div class="bg-error col-span-2">
-          <div class="m-4">
-            <ul class="w-full steps">
-              <li class="step step-primary">Detalles</li>
-              <li class="step step-primary">Envío</li>
-              <li class="step step-primary">Pago</li>
-              <li class="step">Confirmar</li>
-            </ul>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="flex-col justify-center hero-content lg:flex-row shadow-2xl card">
+        <div class=" bg-red-900 p-8">
+          <Step step={step} />
+          <div class="divider">
+          <BodySelector step={step} />
+
+          <div class="flex flex-row bg-gray-50 ">
+            <TacoButton
+              moreClasses="w-1/2 p-1"
+              bgColor="300"
+              callBack={() => onGoBack()}
+              text="Regresar"
+            />
+            <TacoButton
+              moreClasses="w-1/2 p-1"
+              bgColor="300"
+              callBack={() => onNext()}
+              text="Continuar"
+            />
           </div>
         </div>
-        <div class="bg-primary row-span-2 col-span-2">1</div>
-        <div class="bg-secondary row-span-3">
-          <div class="flex flex-col w-full">
-            <div class="mt-2">
-              <h1>RESUMEN DE ORDEN</h1>
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div class="card-body">
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                class="input input-bordered"
+              />
             </div>
-            <div class="divider"></div>
-            <div class="bg-info">
-              <div class="bg-red-600">
-                <div class="flex flex-row">
-                  <div>
-                    <div>
-                      <div class="card-body">
-                        <h2 class="card-title">No Images</h2>
-                        <p class="text-justify">
-                          Rerum reiciendis beatae tenetur excepturi aut pariatur
-                          est eos. Sit sit necessitatibus veritatis sed
-                          molestiae voluptates incidunt iure sapiente.
-                        </p>
-                        <h2 class="card-title mb-0 mt-3">Cantidad: 10</h2>
-                        <div class="card-actions m-0 ">
-                          <button class="btn btn-error m-0">
-                            <MdDelete color="white" />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="divider m-0"></div>
-              <div class="bg-green-700 p-2">
-                <div class="form-control">
-                  <label class="label">
-                    <span class="label-text">Cupón de Descuento:</span>
-                  </label>
-                  <div class="relative">
-                    <input
-                      type="text"
-                      placeholder="PJ: EXRT5"
-                      class="w-full  input input-primary input-bordered"
-                    />
-                    <button class="absolute top-0 right-0 rounded-l-none btn btn-warning">
-                      APLICAR
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div class="divider m-0 "></div>
-                <div class="bg-yellow-500 grid place-items-center">
-                  <h1>TOTAL:</h1>
-                  <h1>$2552</h1>
-                </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                class="input input-bordered"
+              />
+              <label class="label">
+                <a href="#" class="label-text-alt">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div class="form-control mt-6">
+              <input type="button" value="Login" class="btn btn-primary" />
             </div>
           </div>
         </div>
       </div>
     </div>
   );
+}
+
+function Step({ step }) {
+  const completed = "step-primary";
+  const start = "step ";
+  return (
+    <ul class="w-full steps my-5">
+      <li class={step >= 1 ? start + completed : start}>Detalles</li>
+      <li class={step >= 2 ? start + completed : start}>Envío</li>
+      <li class={step >= 3 ? start + completed : start}>Pago</li>
+      <li class={step >= 4 ? start + completed : start}>Confirmar</li>
+    </ul>
+  );
+}
+
+function BodySelector({ step }) {
+  switch (step) {
+    case 2:
+      return <Delivery />;
+    case 3:
+      return <Payment />;
+    case 4:
+      return <Confirmation />;
+    default:
+      return <Details />;
+  }
 }
