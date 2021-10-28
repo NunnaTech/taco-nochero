7470af15e173da92dd697255ed610e985db9c64c
JairDavid <jairvasmar@gmail.com>
2021-10-28 11:33:39 -0500

    added more assets and fix to landing borders

--- src/views/landing_pages/SectionBody.jsx
+++ src/views/landing_pages/SectionBody.jsx
@@ -4,15 +4,14 @@ import Platillo from '../../assets/img/platillo.jpg'
 const SectionBody = () => {
     return (
         <>
-
-            <section className="hero h-130 py-20">
+            <section className="hero h-130 py-14 lg:px-20">
                 <div className="flex-col hero-content lg:flex-row">
                     <div>
                         <img src={Platillo} className="object-contain md:object-scale-down sm:object-scale-down" />
                     </div>
 
                     <div className="md:container md:mx-auto lg:container lg:mx-auto">
-                        <h1 className="mb-5 text-4xl">
+                        <h1 className="mb-2 text-4xl">
                             Encuentra
                         </h1>
                         <h1 className="mb-5 text-5xl font-bold">
@@ -24,22 +23,24 @@ const SectionBody = () => {
                     </div>
                 </div>
             </section>
-            <section className="hero h-120 ">
+            <section className="hero h-130 lg:px-20">
                 <div className="flex-col hero-content lg:flex-row-reverse">
                     <div >
                         <img src={Cerveza} className="object-contain md:object-scale-down sm:object-scale-down" />
                     </div>
 
-                    <div className="md:container md:mx-auto lg:container lg:mx-auto">
-                        <h1 className="mb-5 text-4xl">
-                            Disfruta
-                        </h1>
-                        <h1 className="mb-5 text-5xl font-bold">
-                            Los descuentos
-                        </h1>
-                        <p className="mb-5">
-                            Que tenemos para tí cada semana todos los días del año.
-                        </p>
+                    <div className="md:container md:mx-auto lg:container lg:mx-auto flex justify-end">
+                        <div className="">
+                            <h1 className="mb-2 text-4xl">
+                                Disfruta
+                            </h1>
+                            <h1 className="mb-5 text-5xl font-bold">
+                                Los descuentos
+                            </h1>
+                            <p className="mb-5">
+                                Que tenemos para tí cada semana todos los días del año.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </section>
