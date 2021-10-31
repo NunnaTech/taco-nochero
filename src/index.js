20d83c25dae554bb2897703da74088e077b86afa
JairDavid <jairvasmar@gmail.com>
2021-10-30 21:14:33 -0500

    fix to hector branch v2

--- src/index.js
+++ src/index.js
@@ -2,21 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "../src/assets/css/tailwind.css";
-<<<<<<< HEAD
-import LandingPage from './views/landing_pages/LandingPage'
-=======
 import Menu from './views/landing_pages/Dashboard';
->>>>>>> f63944fa7bd5cb69d5c383afa8807d32ef5098a5
 import reportWebVitals from './reportWebVitals';
 import ShoppingCart from "./views/shopping_cart/ShoppingCart";
 
 ReactDOM.render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <ShoppingCart />
-=======
     <Menu />
->>>>>>> f63944fa7bd5cb69d5c383afa8807d32ef5098a5
   </React.StrictMode>,
   document.getElementById('root')
 );
