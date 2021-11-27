2732db3c3517e9a9d37d2bf06d15b8bfcfb14dfd
JairDavid <jairvasmar@gmail.com>
2021-11-27 12:16:19 -0600

    added menu button on the navbar, fixed some visual bugs into datatable component

--- src/views/landing_pages/Menu.jsx
+++ src/views/landing_pages/Menu.jsx
@@ -50,11 +50,18 @@ const Menu = () => {
                             bgColor="400"
                             moreClasses="font-semibold"
                             callBack={() => {
-                            
+
                             }}
                             text="Iniciar sesión"
                         />
                     </Link>
+                </div>
+                <div className="flex-none lg:hidden ">
+                    <button className="btn btn-square btn-ghost">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </>
